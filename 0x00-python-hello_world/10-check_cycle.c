fc54a1feda479cc40c151870a1f92e8721afd9e2
Muideen27 <ilorimuideen0000@gmail.com>
2023-01-06 07:15:32 +0000

    Although that way may not be obvious at first unless you're Dutch.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ *check_cycle - checks whether there is a loop
+ *utilized floyed-cycle algorithm
+ *@list: head of the linked least
+ *Return: 1 if there is a loop, 0 if there isn't
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = NULL, *slow = NULL;
+
+	if (!list)
+		return (0);
+	fast = list->next;
+	slow = list;
+
+	while (fast && fast->next)
+	{
+		/*if there is a loop they would crush*/
+		if (fast == slow)
+			return (1);
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return (0);
+}
