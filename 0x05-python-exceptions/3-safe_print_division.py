935f5b3fea2505252b40c52c5626a17a68130c78
Muideen27 <ilorimuideen0000@gmail.com>
2023-01-23 12:24:44 +0000

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-def list_division(my_list_1, my_list_2, list_length):
-    count = 0
-    i = 0
-    n_list = []
-    for i in range(list_length):
-        try:
-            r = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("wrong type")
-            r = 0
-        except ZeroDivisionError:
-            print("division by 0")
-            r = 0
-        except IndexError:
-            print("out of range")
-            r = 0
-        finally:
-            n_list.append(r)
-    return n_list
+
+
+def safe_print_division(a, b):
+    """
+    divides two integers and prints the result
+    catches divide by zero exception
+    """
+    try:
+        res = a / b
+    except:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return res
