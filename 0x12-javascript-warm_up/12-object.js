5a10c1c0d1648d3e78744ac3e91c625c51d1367e
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 07:39:52 -0700

    git push

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Muideen27
+// 12-object.js
+// Update the script to replace the value 12 with 89
+
+const myObject = {
+  [type]: 'object',
+  [value]: 12
+};
+console.log(myObject);
+myObject['value'] = 89;
+console.log(myObject);
