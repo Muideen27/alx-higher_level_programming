474b06dd6f2062b34fbd61648a0b282660722f59
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 08:32:25 -0700

    Add file

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,13 +3,8 @@
 // 9-add.js
 // write a script that prints the addition of 2 interger
 
-fuction addNumber (a, b) {
-	return a + b
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-	if (isNaN(arg1) || isNaN(arg2)) {
-		console.log("Missing or invalid argument(s)");
-	} else {
-		const result = addNumber(arg1, arg2);
-		console.log(result);
-	}
+function add (a, b) {
+  return a + b;
+};
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
