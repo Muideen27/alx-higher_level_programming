598688c94c4daab0ce41322abbf752e35364d0a9
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 04:33:50 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,9 +7,9 @@
 const count = process.argv.length;
 
 if (count === 0) {
-console.log('No argument')
+console.log('No argument');
 } else if (count === 1) {
-console.log('Arguments passed')
+console.log('Arguments passed');
 } else {
-console.log('Arguments found')
+console.log('Arguments found');
 }
