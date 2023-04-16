855b7992555fd22ff0356dd92178e7ff5f4a5db6
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 04:05:23 -0700

    First constant, first print

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -4,8 +4,8 @@
 // 0-javascript_is_amazing.js
 // Write a script that prints "JavaScript is amazing";
 
-const myVar = 'JavaScript is amazing'; 
+const myVar = 'JavaScript is amazing';
 // assigning a constant variable (const) for a string.
 
-console.log(myVar); 
+console.log(myVar);
 // printing the output in the console.
