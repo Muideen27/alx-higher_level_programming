af9a30d27fff6f56aaace58189527a7450b10f1b
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 06:58:20 -0700

    Add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Muideen27
+// 9-add.js
+// write a script that prints the addition of 2 interger
+
+fuction addNumber (a, b) {
+	return a + b;
+}
+console.log(addNumber(Number(process.argv[2]), Number(process.argv[3])));
