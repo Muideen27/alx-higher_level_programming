55712fcf4eded3e1d2ed6ece25c0280f12b2d5bc
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 07:42:06 -0700

    All

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -4,9 +4,9 @@
 // Update the script to replace the value 12 with 89
 
 const myObject = {
-  [type]: 'object',
-  [value]: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
-myObject['value'] = 89;
+myObject.value = 89;
 console.log(myObject);
