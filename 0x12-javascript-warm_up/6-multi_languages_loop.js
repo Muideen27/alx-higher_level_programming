005736cd9d5d92abfcc535b5a861b36270813121
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 06:27:52 -0700

    Loops to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Muideen27
 // 6-multi_languages_loop.js
-// Write a script that prints 3 lines: (like 1-multi_languages.js) 
+// Write a script that prints 3 lines: (like 1-multi_languages.js)
 // but by using an array of string and a loop
 
-for (let multiLanguage of ['C is fun', 'Python is cool', 'JavaScript is amazing'] {
-	console.log(multiLanguage);
+for (let multiLanguage of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+console.log(multiLanguage);
 };
