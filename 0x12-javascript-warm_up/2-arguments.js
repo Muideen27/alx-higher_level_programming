2d8577e91f965bc3b398eb3aee3e459a1df2af78
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 04:39:46 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -6,9 +6,9 @@
 
 const count = process.argv.length;
 
-if (count === 0) {
+if (count === 2) {
 console.log('No argument');
-} else if (count === 1) {
+} else if (count === 3) {
 console.log('Argument found');
 } else {
 console.log('Arguments found');
