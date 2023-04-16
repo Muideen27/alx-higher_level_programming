328ed7d77ec8e198d846b04c471bf8a7b0e4fc5c
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 04:55:27 -0700

    Value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Muideen27
+// Write a script that prints the first argument passed to it
+
+const arg = precess.arg[2];
+
+if (arg === NaN) {
+	console.log('No argument');
+}else {
+	console.log(arg);
+};
