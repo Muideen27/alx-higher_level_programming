eee0f698bd4d1c86116ceae11693f7d797e9959d
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 04:29:43 -0700

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Muideen27
+// 2-arguments.js
+// Write a script that prints a message depending of the number of arguments passed
+
+const count = process.argv.length;
+
+if (count === 0) {
+console.log('No argument')
+} else if (count === 1) {
+console.log('Arguments passed')
+} else {
+console.log('Arguments found')
+}
