ad86bd661d64630f8d9b09a3b6ab8d5fa1a65917
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 05:18:34 -0700

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,4 +4,4 @@
 // 4-concat.js
 // Write a script that prints two arguments passed to it, in the following format: “ is ”
 
-console.log(`${prosess.arg[2]} is ${process.arg[3]}`);
+console.log(`${prosess.argv[2]} is ${process.argv[3]}`);
