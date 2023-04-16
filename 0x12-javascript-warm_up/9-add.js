286395561c18895380610502370e787f686a4127
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 07:07:08 -0700

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,6 +4,12 @@
 // write a script that prints the addition of 2 interger
 
 fuction addNumber (a, b) {
-	return a + b;
-}
-console.log(addNumber(Number(process.argv[2]), Number(process.argv[3])));
+	return a + b
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+	if (isNaN(arg1) || isNaN(arg2)) {
+		console.log("Missing or invalid argument(s)");
+	} else {
+		const result = addNumber(arg1, arg2);
+		console.log(result);
+	}
