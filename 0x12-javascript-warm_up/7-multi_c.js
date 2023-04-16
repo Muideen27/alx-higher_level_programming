baa5d2261ce69c5a681ffa84e9d62bb6854e138e
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-16 06:46:02 -0700

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Muideen27
+// 7-multi_c.js
+// Write a script that prints x times “C is fun”
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(X)) {
+	console.log('Missing number of occurences');
+} else {
+	for (let i = 0; i < x; i++) {
+		console.log('C is fun');
+	}
+}
