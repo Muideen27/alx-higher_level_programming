21f43a377a39e1b721324e0b9901c07ef531ae22
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-18 12:32:15 +0000

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+
+"""
+import MySQLdb
+import sys
+"""
+Taking argument and connecting to MyQSL server, it executes the SELECT command to retreive rows form states and sort in ascending order 
+"""
+if __name__ == '__main__':
+    username, password, database = sys.argv[1:]
+    
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    for state in cur.fetchall():
+        print(state)
+
+    cur.close()
+    db.close()
