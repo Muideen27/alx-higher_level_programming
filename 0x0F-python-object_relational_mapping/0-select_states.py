c4f558fbd12234d8adc7194788ef7a6c73380f28
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-18 12:17:25 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,10 +11,6 @@ Taking arguments as input
 if __name__ == '__main__':
     username, password, database = sys.argv[1:]
 
-"""
-Connecting to MySQL server running on localhost port 3306 using MySQLdb module
-"""
-
     db = MySQLdb.connect(
             host='localhost',
             port=3306,
@@ -23,9 +19,6 @@ Connecting to MySQL server running on localhost port 3306 using MySQLdb module
             db=database
         )
     cur = db.cursor()
-"""
-Executing SQL SELECT command to retrieve all the rows from the states table in ascending order by id.
-"""
 
     cur.execute('SELECT * FROM states ORDER BY id ASC')
 
