85aa2cc4a4914e08a242058cce443646fa58d0bd
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-20 17:09:14 +0000

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-"""
-Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 
-"""
-import MySQLdb
-import sys
-"""
-Taking argument and connecting to MyQSL server, it executes the SELECT command to retreive rows form states and sort in ascending order
-"""
-if __name__ == '__main__':
-    username, password, database = sys.argv[1:]
-    
-    db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+"""Script lists all states with a name starting with N from database
+Takes three arguments:
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states\ WHERE name LIKE 'N%'\ ORDER BY id ASC"
-    state = cur.fetchall()
+mysql username
+mysql password
+database name
 
-    for row in state:
-        print(row)
+Connects to default host (localhost) and port (3306)
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+     c = db.cursor()
+
+      c.execute("""SELECT * FROM states WHERE name like "N%" ORDER BY states.id ASC""")
+
+      rows = c.fetchall()
+      for row in rows:
+          if row[1][0] == 'N':
+               print(row)
