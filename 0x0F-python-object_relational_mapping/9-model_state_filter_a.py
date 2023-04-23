f534d6aa959d0a060ea0331bedb3ff0956c79116
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-23 14:24:24 +0000

    Contains a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,8 +11,10 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
+
     engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
     session = Session()
+
 for instance in session.query(State).filter(State.name.contains('a')):
     print('{0}: {1}'.format(instance.id, instance.name))
