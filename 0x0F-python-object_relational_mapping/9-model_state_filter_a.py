b0f1dfc96430e8ad0f93d7a23517dc2686341000
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-23 14:15:13 +0000

    Contains a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,6 +14,5 @@ if __name__ == "__main__":
     engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for instance in session.query(State).filter(State.name.contains('a')):
-        print('{0}: {1}'.format(instance.id, instance.name))
+for instance in session.query(State).filter(State.name.contains('a')):
+    print('{0}: {1}'.format(instance.id, instance.name))
