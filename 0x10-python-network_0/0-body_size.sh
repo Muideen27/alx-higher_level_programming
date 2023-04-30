5b07456cf198ee0a55ed7ff5c1d405ad045cd61d
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-30 08:28:04 +0000

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-
 # takes in a URL, sends a request to that URL, and 
 # displays the size of the body of the response
 
-# checking if any aurgument received
-if [ $# -eq 0 ]; then
-	  echo "Please provide a URL as an argument"
-	    exit 1
-fi
+I "$1" | grep -i Content-Length | cut -d ' ' -f2
 
-# variable url
-url=$1
-response=$(curl -sI $url)
-content_length=$(echo "$response" | awk '/Content-Length/ {print $2}' | tr -d '\r')
-
-if [ -z "$content_length" ]; then
-	  echo "Unable to determine size of response body"
-  else
-	    echo "Size of response body: $content_length bytes"
-fi
