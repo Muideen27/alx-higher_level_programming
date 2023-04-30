3346e6da4b45d7f119bbb28690b7dc1d18372989
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-30 10:58:01 -0700

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-#!/bin/bash
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:HolbertonSchool"
+# A Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin:HolbertonSchool" > /dev/null
