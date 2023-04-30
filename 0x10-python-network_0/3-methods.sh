120415ec1079da22d027b5595827a599ce423aaa
Muideen27 <ilorimuideen0000@gmail.com>
2023-04-30 03:29:21 -0700

    cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -i -X OPTIONS "$URL" | awk '/Allow/ {print $2}'
+curl -sI -X OPTIONS "$1" | grep Allow | cut -d ' ' -f1 --complement
