9636110420b4768c73175ea139b9c346fc483944
Muideen27 <ilorimuideen0000@gmail.com>
2023-05-02 21:43:54 +0000

    updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -sLX PUT -H "origin:Best School" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "origin:School" -d "user_id=98" 0.0.0.0:5000/catch_me
