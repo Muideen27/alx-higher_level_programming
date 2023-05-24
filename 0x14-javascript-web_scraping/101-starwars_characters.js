124a9af39f8a04a4de5181af96d57d38a0074edb
Muideen27 <ilorimuideen0000@gmail.com>
2023-05-24 08:25:16 +0000

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,35 @@
 #!/usr/bin/node
-
+// Prints the title of a Star Wars movie where the episode number matches a given integer.
+const argv = require('process').argv;
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-let def = {};
-request(url, function (error, response, body) {
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
+
+// Request module is async and for us to have the list in order we need to
+// make our requests in a synchronous manner
+// Recursive functions to make synchronous requests
+function makeRequests (urls) {
+  const url = urls.shift();
+  request(url, (error, response, body) => {
+    // If the list is not empty do some recursion
+    if (urls.length) {
+      makeRequests(urls);
+    }
+    if (error) {
+      console.log(error);
+    }
+
+    console.log(JSON.parse(body).name);
+  });
+}
+
+// Request the film data
+request(url, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const abc = JSON.parse(body);
-    abc.characters.forEach(function (item, index, array) {
-      request(item, function (error, response, content) {
-        if (error) {
-          console.log(error);
-        } else {
-          def = JSON.parse(content);
-          console.log(def.name);
-        }
-      });
-    });
   }
+
+  const characters = JSON.parse(body).characters;
+  // Make a request for each character
+  makeRequests(characters);
 });
