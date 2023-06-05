0639e713d324704ee377c9d076b8c7cd6e597b7c
Muideen27 <ilorimuideen0000@gmail.com>
2023-06-05 08:19:16 +0000

    error fixed

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // Muideen27
 // 5-to_integer.js
-// Write a script that prints My number: <first argument converted in integer> 
+// Write a script that prints My number: <first argument converted in integer
 // if the first argument can be converted to an integer
 
 const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log(`My number: ${num}`);
-};
+  console.log(`My number: ${num}`);
+}
