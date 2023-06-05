d96415675a30df024a48c1cfc423c16c4b048f39
Muideen27 <ilorimuideen0000@gmail.com>
2023-06-05 07:44:06 +0000

    updated 2-argument file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,11 @@
 
 // Muideen27
 // 2-arguments.js
-// Write a script that prints a message 
+// Write a script that prints a message
 // depending of the number of arguments passed
 
 const count = process.argv.length;
+
 if (count === 2) {
 console.log('No argument');
 } else if (count === 3) {
