438bc2efe58cb14cf42956f3be9d13ebb08054db
Muideen27 <ilorimuideen0000@gmail.com>
2023-06-05 08:45:36 +0000

    bub fixed 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,14 @@
 // 11-second_biggest.js
 // write a scrip thar searches the second biggest integer in the list or arguments
 
-const args = process.argv.slice(2).map(Number); 
+const args = process.argv.slice(2).map(Number);
 // Get arguments and convert them to numbers
 
 if (args.length === 0) {
-	console.log(0); // If no argument passed, print 0
+  console.log(0); // If no argument passed, print 0
 } else if (args.length === 1) {
-	console.log(0); // If the number of arguments is 1, print 0
+  console.log(0); // If the number of arguments is 1, print 0
 } else {
-	const sortedArgs = args.sort((a, b) => b - a); // Sort arguments in descending order
-	console.log(sortedArgs[1]); // Print the second biggest integer
+  const sortedArgs = args.sort((a, b) => b - a); // Sort arguments in descending order
+  console.log(sortedArgs[1]); // Print the second biggest integer
 }
-
